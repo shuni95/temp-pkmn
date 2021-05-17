a167b348325f164266d9fe94aff533ff48adc7d4
Junior Claudio <shuni95@protonmail.com>
2021-05-16 21:48:49 -0500

    Adding StarterContent folder from ThirdPerson project Setting up PokemonCharacter to handle multiplayer with MovementActor - Replication set on CurrentHealth and Defeated properties

---
+++ Content/Characters/PokemonCharacter.cpp
@@ -0,0 +1,132 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PokemonCharacter.h"
+#include "Net/UnrealNetwork.h"
+#include "Engine/Engine.h"
+#include "MovementActor.h"
+
+// Sets default values
+APokemonCharacter::APokemonCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Initialize the player's Health	
+	MaxHealth = 100.0f;
+	CurrentHealth = MaxHealth;
+
+    // Initialize movement class
+    MovementClass = AMovementActor::StaticClass();
+    // Initialize movement rate
+    MovementRate = 0.25f;
+    bIsPerformingAMovement = false;
+}
+
+// Called when the game starts or when spawned
+void APokemonCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APokemonCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APokemonCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    check(PlayerInputComponent);
+    // Handle perfoming movements
+    PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &APokemonCharacter::StartMovement);
+}
+
+void APokemonCharacter::GetLifetimeReplicatedProps(TArray <FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	// Replicate current health.
+	DOREPLIFETIME(APokemonCharacter, CurrentHealth);
+}
+
+void APokemonCharacter::OnHealthUpdate()
+{
+    //Client-specific functionality
+    if (IsLocallyControlled())
+    {
+        FString healthMessage = FString::Printf(TEXT("You now have %f health remaining."), CurrentHealth);
+        GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, healthMessage);
+
+        if (CurrentHealth <= 0)
+        {
+            FString deathMessage = FString::Printf(TEXT("You have been killed."));
+            GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, deathMessage);
+        }
+    }
+
+    // Server-specific functionality
+    if (GetLocalRole() == ROLE_Authority)
+    {
+        FString healthMessage = FString::Printf(TEXT("%s now has %f health remaining."), *GetFName().ToString(), CurrentHealth);
+        GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, healthMessage);
+    }
+
+    // Functions that occur on all machines. 
+    /*
+        Any special functionality that should occur as a result of damage or death should be placed here.
+    */
+}
+
+void APokemonCharacter::OnRep_CurrentHealth()
+{
+    OnHealthUpdate();
+}
+
+void APokemonCharacter::SetCurrentHealth(float healthValue)
+{
+    if (GetLocalRole() == ROLE_Authority)
+    {
+        CurrentHealth = FMath::Clamp(healthValue, 0.f, MaxHealth);
+        OnHealthUpdate();
+    }
+}
+
+float APokemonCharacter::TakeDamage(float DamageTaken, struct FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+    float damageApplied = CurrentHealth - DamageTaken;
+    SetCurrentHealth(damageApplied);
+    return damageApplied;
+}
+
+void APokemonCharacter::StartMovement() 
+{
+    if (!bIsPerformingAMovement) {
+        bIsPerformingAMovement = true; 
+        UWorld* World = GetWorld();
+        World->GetTimerManager().SetTimer(MovementTimer, this, &APokemonCharacter::StopMovement, MovementRate, false);
+        HandleMovement();
+    }
+}
+
+void APokemonCharacter::StopMovement()
+{
+    bIsPerformingAMovement = false;
+}
+
+void APokemonCharacter::HandleMovement_Implementation()
+{
+    FVector spawnLocation = GetActorLocation() + (GetControlRotation().Vector() * 100.0f) + (GetActorUpVector() * 50.0f);
+    FRotator spawnRotation = GetControlRotation();
+
+    FActorSpawnParameters spawnParameters;
+    spawnParameters.Instigator = GetInstigator();
+    spawnParameters.Owner = this;
+
+    AMovementActor* spawnedMovement = GetWorld()->SpawnActor<AMovementActor>(spawnLocation, spawnRotation, spawnParameters);
+}
---
+++ Content/Characters/PokemonCharacter.h
@@ -0,0 +1,87 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PokemonCharacter.generated.h"
+
+UCLASS()
+class TEMPPKMN_API APokemonCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APokemonCharacter();
+
+	/** Getter for Max Health.*/
+	UFUNCTION(BlueprintPure, Category = "Health")
+	FORCEINLINE float GetMaxHealth() const { return MaxHealth; }
+
+	/** Getter for Current Health.*/
+	UFUNCTION(BlueprintPure, Category = "Health")
+	FORCEINLINE float GetCurrentHealth() const { return CurrentHealth; }
+
+	// Setter for Current Health. Clamps the value between 0 and MaxHealth and calls OnHealthUpdate. Should only be called on the server.
+	UFUNCTION(BlueprintCallable, Category = "Health")
+	void SetCurrentHealth(float healthValue);
+
+	// Event for taking damage. Overridden from APawn.
+	UFUNCTION(BlueprintCallable, Category = "Health")
+	float TakeDamage(float DamageTaken, struct FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// The player's maximum health. This is the highest that their health can be, and the value that their health starts at when spawned.
+	UPROPERTY(EditDefaultsOnly, Category = "Health")
+	float MaxHealth;
+
+	// The player's current health. When reduced to 0, they are considered dead.
+	UPROPERTY(ReplicatedUsing = OnRep_CurrentHealth)
+	float CurrentHealth;
+
+	// RepNotify for changes made to current health.
+	UFUNCTION()
+	void OnRep_CurrentHealth();
+
+	// Response to health being updated. Called on the server immediately after modification, and on clients in response to a RepNotify
+	void OnHealthUpdate();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Gameplay|Projectile")
+	TSubclassOf<class AMovementActor> MovementClass;
+
+	// Delay between movements in seconds
+	UPROPERTY(EditDefaultsOnly, Category = "Gameplay")
+	float MovementRate;
+
+	// If true, we are in the process of throwing movements
+	bool bIsPerformingAMovement;
+
+	// Function for beggining a movement
+	UFUNCTION(BlueprintCallable, Category = "Gameplay")
+	void StartMovement();
+
+	// Function for ending a movement
+	UFUNCTION(BlueprintCallable, Category = "Gameplay")
+	void StopMovement();
+
+	// Server function for spawning movements
+	UFUNCTION(Server, Reliable)
+	void HandleMovement();
+
+	// Timer for Movement
+	FTimerHandle MovementTimer;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Property replication
+	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+};
